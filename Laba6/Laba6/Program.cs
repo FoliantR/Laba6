44ddc96d19a4604bf456a1e67ef0dc8acc0b36a2
Stefan <s.nowgorodsky@yandex.ru>
2020-05-14 13:11:59 +0300

    Конкретно так сделал, но не совсем

--- Laba6/Laba6/Program.cs
+++ Laba6/Laba6/Program.cs
@@ -12,31 +12,52 @@ namespace Laba6
 {
     class Program
     {
+        static int ResourceCPU = 100, ResourceGPU = 100;
+        const int ResourceCPUForGame = 16, ResourceGPUForGame = 40;
+        const int ResourceCPUForGrpaphic = 25, ResourceGPUForGraphic = 30;
+
         static void Main(string[] args)
         {
-            bool exit = true;
+            Thread paint = new Thread(new ThreadStart(GraphicRedactor));
+            Thread game = new Thread(new ThreadStart(ComputerGame));
 
-            List<Thread> potoki = new List<Thread>();
-            Thread t = Thread.CurrentThread;
-            potoki.Add(t);
-            t.Name = "Ujh";
-            do
+            game.Start();
+            paint.Start();
+
+            for (int i = 0; i < 10; i++)
             {
-                Console.Clear();
-                foreach (var item in potoki)
-                {
-                    Console.WriteLine($"{item.Name} {item.IsAlive} {item.ThreadState} {Thread.GetDomain().FriendlyName}\n");
-                }
-                Console.WriteLine("6 - Выход");
-                switch (Convert.ToInt32(Console.ReadLine()))
-                {
-                    case 6:
-                        exit = false;
-                        break;
-                    default:
-                        break;
-                }
-            } while (exit);
+                Console.WriteLine($"Сколько свободно CPU{ResourceCPU}% GPU{ResourceGPU}%");
+                Thread.Sleep(3000);
+            }
+        }
+        public static void ComputerGame()
+        {
+            ResourceCPU -= ResourceCPUForGame;
+            ResourceGPU -= ResourceGPUForGame;
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"Что то проиходит в игре ресурсы использующиеся " +
+                    $"CPU {ResourceCPUForGame}%" +
+                    $"GPU {ResourceGPUForGame}% ");
+                Thread.Sleep(1000);
+            }
+            ResourceCPU += ResourceCPUForGame;
+            ResourceGPU += ResourceGPUForGame;
+        }
+        public static void GraphicRedactor()
+        {
+            ResourceCPU -= ResourceCPUForGrpaphic;
+            ResourceGPU -= ResourceGPUForGraphic;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Что то проиходит в граическом редокторе ресурсы использующиеся " +
+                    $"CPU {ResourceCPUForGrpaphic}%" +
+                    $"GPU {ResourceGPUForGraphic}% ");
+                Thread.Sleep(600);
+            }
+            Console.WriteLine("Граический редактор завершил свою работу");
+            ResourceCPU += ResourceCPUForGrpaphic;
+            ResourceGPU += ResourceGPUForGraphic;
         }
     }
 }
